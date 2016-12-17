78a2e3fa6ef93920425693050ed43e3c0b04ce81
giuseppetanzi <giuse00@gmail.com>
2016-12-17 21:51:42 +0100

    Update jenkins-swarm-slave.sh

--- slave/jenkins-swarm-slave.sh
+++ slave/jenkins-swarm-slave.sh
@@ -13,7 +13,8 @@ echo "Running Jenkins Swarm Plugin...."
 JAR=`ls -1 /home/jenkins/swarm-client-*.jar | tail -n 1`
 
 if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
+#PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
+PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
 fi
 
 echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
