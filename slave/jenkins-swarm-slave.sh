204c725e06b5530cd82f085a021b7572dff25d0e
giuseppetanzi <giuse00@gmail.com>
2016-12-18 15:47:58 +0100

    Update jenkins-swarm-slave.sh

--- slave/jenkins-swarm-slave.sh
+++ slave/jenkins-swarm-slave.sh
@@ -3,18 +3,8 @@
 master_username=${JENKINS_USERNAME:-"admin"}
 master_password=${JENKINS_PASSWORD:-"password"}
 slave_executors=${EXECUTORS:-"1"}
-export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 source /usr/local/bin/generate_container_user
-
-function get_server_num() {
-  echo $(echo $DISPLAY | sed -r -e 's/([^:]+)?:([0-9]+)(\.[0-9]+)?/\2/')
-}
-
-function shutdown {
-  kill -s SIGTERM $NODE_PID
-  wait $NODE_PID
-}
 
 echo "Running Jenkins Swarm Plugin...."
 
@@ -26,16 +16,4 @@ if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
 PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors} -labels ${SLAVE_LABEL}"
 fi
 
-echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@" in xvfb environment
-
-SERVERNUM=$(get_server_num)
-echo SERVERNUM $SERVERNUM
-
-rm -f /tmp/.X*lock
-
-xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@" &
-NODE_PID=$!
-
-trap shutdown SIGTERM SIGINT
-wait $NODE_PID
+java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
